ad70a3db3c04c9fd72dde16362c4eaba6e6e2e6c
cachac <carlos.echc11@gmail.com>
2019-09-01 00:33:15 -0600

    CRUD api

--- vuedemo/src/API/model/userModel.js
+++ vuedemo/src/API/model/userModel.js
@@ -1,4 +1,3 @@
-//import mysqlConnection from "../database";
 var mysqlConnection = require("../database");
 
 //object constructor
@@ -6,21 +5,81 @@ const User = function(user) {
   this.id = user.id;
   this.nombre = user.nombre;
   this.cedula = user.cedula;
+  this.correo = user.correo;
+  this.usuario = user.usuario;
+  this.passwd = user.passwd;
+  this.empresa = user.empresa;
+  this.fechaCreacion = user.fechaCreacion;
 };
 
-User.getAll = function(result) {
+User.readAll = function(result) {
   mysqlConnection.query(
     "select * from control_acceso_cdc_dbp.usuario_n limit 10;",
     function(err, res) {
       if (err) {
         console.log("error: ", err);
-        result(null, err);
+        result(err, null);
       } else {
-        console.log("Sending DATA...");
         result(null, res);
       }
     }
   );
+};
+
+User.readById = function(id, result) {
+  mysqlConnection.query(
+    "SELECT id, usuario, passwd, cedula, nombre, correo, empresa, estado FROM usuario_n WHERE id = ?",
+    id,
+    function(err, res) {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+      } else {
+        result(null, res);
+      }
+    }
+  );
+};
+
+User.create = function(_new, result) {
+  const uuid = require("uuid/v1");
+  _new.id = uuid();
+  mysqlConnection.query("INSERT INTO usuario_n set ?", _new, function(err) {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+    } else {
+      result(null, _new);
+    }
+  });
+};
+
+User.update = function(id, _obj, result) {
+  mysqlConnection.query(
+    "UPDATE usuario_n SET ? WHERE id = ?",
+    [_obj, id],
+    function(err) {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+      } else {
+        result(null, _obj);
+      }
+    }
+  );
+};
+
+User.delete = function(id, result) {
+  mysqlConnection.query("DELETE FROM usuario_n WHERE id = ?", id, function(
+    err
+  ) {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+    } else {
+      result(null, id);
+    }
+  });
 };
 
 module.exports = User;
--- vuedemo/src/API/routes/userRoute.js
+++ vuedemo/src/API/routes/userRoute.js
@@ -1,5 +1,13 @@
 module.exports = function(app) {
   const controller = require("../controllers/userController");
   // routes
-  app.route("/users").get(controller.GET_ALL);
+  app
+    .route("/users")
+    .get(controller.READ_ALL)
+    .post(controller.CREATE);
+  app
+    .route("/users/:id")
+    .get(controller.READ_BY_ID)
+    .put(controller.UPDATE)
+    .delete(controller.DELETE);
 };
